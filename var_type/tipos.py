b7db51b050eecb3e65d4f524d6d489f4e7ccf96f
Estudiante <73068175@continental.edu.pe>
2026-03-30 11:22:22 -0500

    Envio de tipos

---
+++ var_type/tipos.py
@@ -0,0 +1,36 @@
+nada = "none"
+cadena = "Hola desde python"
+entero = 10
+suma = entero + 10
+flotante = 3.5 - suma
+lista = [10,20,30,40]
+listaString=["pera",20,"manzana"]
+Diccionario = {"nombre":"Juan",
+               "edad":20,"LugarNac":"Lima", "Curso":"Curso de Phyton"}
+
+
+
+
+
+print(nada)
+print(cadena)
+print(entero)
+print(suma)
+print(flotante)
+print(listaString)
+print(Diccionario)
+
+#Mostrar el tipo de dato
+print(type(nada))
+print(type(cadena))
+print(type(entero))
+print(type(suma))
+print(type(flotante))
+print(type(listaString))
+print(type(Diccionario))
+
+print(cadena, " ",suma)
+print("hola" + "mundo")
+print(f"Hola Roberto tu saldo es de {entero} soles.")
+
+print(f"Tienes {entero} años.")
